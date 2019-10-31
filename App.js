fdb381e4fede424f15b0ecc3a4a5a296d46b671f
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-10-31 17:11:03 +0700

    add menu, reader assets

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View stlye={styles.container}>
+      <View style={styles.navigation}>
+        <Image
+          style={styles.logo}
+          source={require("./assets/src/readery.jpg")}
+        />
+        <Image style={styles.menu} source={require("./assets/src/menu.png")} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#ffff",
-    alignItems: "center",
-    justifyContent: "center"
+    flex: 1
+  },
+  navigation: {
+    backgroundColor: "black",
+    alignItems: "flex-start",
+    justifyContent: "flex-start"
+  },
+  logo: {
+    marginLeft: 10,
+    width: 100,
+    height: 50
+  },
+  menu: {
+    width: 20,
+    height: 20
   }
 });
