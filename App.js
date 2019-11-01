c528d9ddfefe2cbee3dda7a653f1c47e9288c6cc
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-11-01 16:08:20 +0700

    Separate to be a component

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableHighlight } from "react-native";
+import Navigation from './components/Navigation.js';
 
 export default function App() {
   return (
     <View stlye={styles.container}>
-      <View style={styles.navigation}>
-        <Image
-          style={styles.logo}
-          source={require("./assets/src/readery.jpg")}
-        />
-        <Image style={styles.menu} source={require("./assets/src/menu.png")} />
-      </View>
+      <Navigation />
     </View>
   );
 }
@@ -18,19 +13,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1
-  },
-  navigation: {
-    backgroundColor: "black",
-    alignItems: "flex-start",
-    justifyContent: "flex-start"
-  },
-  logo: {
-    marginLeft: 10,
-    width: 100,
-    height: 50
-  },
-  menu: {
-    width: 20,
-    height: 20
   }
 });
