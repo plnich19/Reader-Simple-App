56e901aa18afc75841ae2f27113a4905f78e08b6
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-11-15 13:54:38 +0700

    Revert "Update API.js"
    This reverts commit 3f10b5167ec324f82a6a0e21289c3f2e0f67c81e.

--- API.js
+++ API.js
@@ -1,17 +1,8 @@
 let URL = 'https://us-central1-reader-simple-app.cloudfunctions.net/'
 
-export default (action, email, key, nameth, nameen, author, price, cover, amount) => {
-    if (action == 'addtoCart') {
-        console.log('kyyyyy = ', key)
-        return fetch(URL + action + '?email' + email + '?key=' + key + '?nameth=' + nameth + '?nameen=' + nameen + '?author=' + author + '?price=' + price + '?cover=' + cover + '?amount=' + amount).then((res) => {
-            console.log(res);
-            return res.json()
-        })
-    }
-    else {
-        return fetch(URL + action + '?nameth=' + nameth).then((res) => {
-            console.log(res);
-            return res.json()
-        })
-    }
+export default (action, nameth) => {
+    return fetch(URL + action + '?nameth=' + nameth).then((res) => {
+        console.log(res);
+        return res.json()
+    })
 }
