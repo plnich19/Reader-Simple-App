4d4e8484525c117d3ddeeb235173ddb6dd9cf9ad
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-11-17 01:17:50 +0700

    Update HomeScreen.js

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Image, TouchableHighlight, ScrollView } from "react-native";
+import { StyleSheet, ScrollView } from "react-native";
+
 import Navigation from './Navigation.js';
-import TrendingBar from './TrendingBar.js';
 import HotBook from './HotBook.js';
-import Find from './Find.js';
 
 export default class HomeScreen extends Component {
     render() {
         return (
             <ScrollView style={styles.container}>
                 <Navigation navigation={this.props.navigation} />
-                {/* <TrendingBar /> */}
                 <HotBook navigation={this.props.navigation} />
 
             </ScrollView>
